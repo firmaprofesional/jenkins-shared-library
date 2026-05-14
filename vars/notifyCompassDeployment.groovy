0a4d090327b294857530ff75e7c1f5a4cd181f1c
mllagostera <miquel.llagostera@logalty.com>
2026-05-14 11:32:10 +0200

    refactor & improvement notifyCompassDeployment

--- vars/notifyCompassDeployment.groovy
+++ vars/notifyCompassDeployment.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String componentId, String state, String environment, String startedAt, String completedAt) {
+def call(String compassCloudId,String componentId, String state, String environment, String startedAt, String completedAt, String pipelineName) {
     try {
+        def cloudId = compassCloudId
+        def jobName = pipelineName
 
-      def envCategory = "DEVELOPMENT";
-      if (environment.toLowerCase().contains("prod")) {
-          envCategory = "PRODUCTION"
-      } else if (environment.toLowerCase().contains("qa"))  {
-          envCategory = "STAGING"
-      } else if (environment.toLowerCase().contains("pre")) {
-          envCategory = "STAGING"
-      }
+        def envCategory = "DEVELOPMENT"
+        if (environment.toLowerCase().contains("prod")) {
+            envCategory = "PRODUCTION"
+        } else if (environment.toLowerCase().contains("qa") || environment.toLowerCase().contains("pre")) {
+            envCategory = "STAGING"
+        }
 
         withCredentials([
             usernamePassword(
@@ -19,8 +19,7 @@ def call(String componentId, String state, String environment, String startedAt,
                 passwordVariable: 'COMPASS_TOKEN'
             )
         ]) {
-            def jobName = env.JOB_NAME.replace('"', '\\"')
-            def payload = """{"cloudId":"067e6789-c624-4d93-8e50-fb6f31d8130e","componentId":"${componentId}","event":{"deployment":{"updateSequenceNumber":${env.BUILD_NUMBER},"displayName":"${jobName} #${env.BUILD_NUMBER}","url":"${env.BUILD_URL}","lastUpdated":"${completedAt}","externalEventSourceId":"${jobName}-${environment}","description":"Deploy triggered from Jenkins","deploymentProperties":{"sequenceNumber":${env.BUILD_NUMBER},"state":"${state}","startedAt":"${startedAt}","completedAt":"${completedAt}","pipeline":{"pipelineId":"${jobName}","displayName":"${jobName}","url":"${env.BUILD_URL}"},"environment":{"environmentId":"${environment}","displayName":"${environment}","category":"${envCategory}"}}}}}"""
+            def payload = """{"cloudId":"${cloudId}","componentId":"${componentId}","event":{"deployment":{"updateSequenceNumber":${env.BUILD_NUMBER},"displayName":"${jobName} #${env.BUILD_NUMBER}","url":"${env.BUILD_URL}","lastUpdated":"${completedAt}","externalEventSourceId":"${jobName}-${environment}","description":"Deploy triggered from Jenkins","deploymentProperties":{"sequenceNumber":${env.BUILD_NUMBER},"state":"${state}","startedAt":"${startedAt}","completedAt":"${completedAt}","pipeline":{"pipelineId":"${jobName}","displayName":"${jobName}","url":"${env.BUILD_URL}"},"environment":{"environmentId":"${environment}","displayName":"${environment}","category":"${envCategory}"}}}}}"""
 
             def httpStatus = sh(script: """
                 curl -s -o /dev/null -w "%{http_code}" -X POST \
